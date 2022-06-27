598ed88a9a3e3f2a399fecaa3d3b4558124d5cb6
SusuMantan <suzumantan@gmail.com>
2022-06-27 14:39:26 +0700

    fix register user

--- frontend/lib/user/api.js
+++ frontend/lib/user/api.js
@@ -1,5 +1,15 @@
 import { fetcher } from "../../config/fetcher";
 
+export const registerUser = async (userData) => {
+  const res = await fetcher.post("/auth/register", {
+    email: userData.email,
+    name: userData.name,
+    password: userData.password,
+  });
+
+  return res.data;
+};
+
 export const getMe = async (token) => {
   const res = await fetcher.get("/users/me", {
     headers: {
